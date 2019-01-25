27aac549752e6c8fb16a2fedab35c92f04028753
shin <auseunghyuk@gmail.com>
2019-01-25 11:43:57 +0900

    use placeholder

--- Adder.py
+++ Adder.py
@@ -2,12 +2,20 @@ import  os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 
-node1 = tf.constant(3.0, tf.float32)
-node2 = tf.constant(4.0)
-add_node = tf.add(node1,node2)
+# node1 = tf.constant(3.0, tf.float32)
+# node2 = tf.constant(4.0)
+# add_node = tf.add(node1,node2)
+#
+# sess = tf.Session()
+# print("sess.run(node1,node2):",sess.run([node1,node2]))
+# print("sess.run(add_node):",sess.run(add_node))
 
-sess = tf.Session()
-print("sess.run(node1,node2):",sess.run([node1,node2]))
-print("sess.run(add_node):",sess.run(add_node))
+# 1.BuildGraph
+a=tf.placeholder(tf.float32)
+b=tf.placeholder(tf.float32)
+add_node = a+b
 
-
+# 2. RunGraph & 3. Update variables
+sess=tf.Session()
+print(sess.run(add_node,feed_dict={a:3,b:4.5}))
+print(sess.run(add_node,feed_dict={a:[1,3],b:[2,4]}))
