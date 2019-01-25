9aaf63aa42c77ccbcb4d1b12607dd4b3eb41e165
shin <auseunghyuk@gmail.com>
2019-01-25 13:22:46 +0900

    linear regression by placeholder

--- LinearRegression.py
+++ LinearRegression.py
@@ -3,10 +3,13 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 
 #Build Graph
-x_train = [1,2,3]
-y_train = [1,2,3]
-W = tf.Variable(tf.random_normal([1]),name='weight')
+#x_train = [1,2,3]
+#y_train = [1,2,3]
+x_train = tf.placeholder(tf.float32)
+y_train = tf.placeholder(tf.float32)
+W = tf.Variable(tf.random_normal([1]),name='weight') #becaues we don't know W,b values
 b = tf.Variable(tf.random_normal([1]),name='bias')
+
 
 hypothesis = x_train*W + b #linear regression
 cost = tf.reduce_mean(tf.square(hypothesis-y_train)) #reduce_mean returns average
@@ -20,6 +23,11 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 for step in range(2001):
-    sess.run(train)
+    #sess.run(train)
+    cost_val, W_val, b_val , _ = \
+        sess.run([cost, W, b, train],
+                 feed_dict={x_train:[1,2,3], y_train:[1,2,3]})
+
     if step % 20 == 0:
-        print(step, sess.run(cost), sess.run(W), sess.run(b))
+        #print(step, sess.run(cost), sess.run(W), sess.run(b))
+        print(step, cost_val, W_val, b_val )
