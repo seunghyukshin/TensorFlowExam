01694525958333e00b2b07982d334f0dbe00910b
shin <auseunghyuk@gmail.com>
2019-01-25 13:41:43 +0900

    add comments

--- LinearRegression.py
+++ LinearRegression.py
@@ -2,29 +2,29 @@ import  os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 
-#Build Graph
+# Build Graph
 #x_train = [1,2,3]
 #y_train = [1,2,3]
 x_train = tf.placeholder(tf.float32)
 y_train = tf.placeholder(tf.float32)
-W = tf.Variable(tf.random_normal([1]),name='weight') #becaues we don't know W,b values
+W = tf.Variable(tf.random_normal([1]),name='weight') # becaues we don't know W,b values
 b = tf.Variable(tf.random_normal([1]),name='bias')
 
 
 hypothesis = x_train*W + b #linear regression
-cost = tf.reduce_mean(tf.square(hypothesis-y_train)) #reduce_mean returns average
-                                                    #square means double multiply
-#GradientDescent
+cost = tf.reduce_mean(tf.square(hypothesis-y_train)) # reduce_mean returns average
+                                                    # square means double multiply
+# GradientDescent
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
 train = optimizer.minimize(cost)
 
-#Run/update Graph
+# Run/update Graph
 sess = tf.Session()
-sess.run(tf.global_variables_initializer())
+sess.run(tf.global_variables_initializer()) # "Variable" requires initializing
 
 for step in range(2001):
     #sess.run(train)
-    cost_val, W_val, b_val , _ = \
+    cost_val, W_val, b_val , _ = \  # "_" means that train value we don't want
         sess.run([cost, W, b, train],
                  feed_dict={x_train:[1,2,3], y_train:[1,2,3]})
 
