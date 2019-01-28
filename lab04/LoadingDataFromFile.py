1e6d84e47de0ebb8ec8165ee13323f104df4504b
shin <auseunghyuk@gmail.com>
2019-01-28 13:51:11 +0900

    Load Data by Queue Runners

--- lab04/LoadingDataFromFile.py
+++ lab04/LoadingDataFromFile.py
@@ -1,6 +1,5 @@
 import  os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-import
 import numpy as np
 
 xy = np.loadtxt('data-01-test-score.csv', delimiter=',', dtype=np.float32)
@@ -10,4 +9,4 @@ y_data = xy[:,[-1]]
 print(x_data.shape, x_data, len(x_data))
 print(y_data.shape, y_data)
 
-X = tf.placeh
+
