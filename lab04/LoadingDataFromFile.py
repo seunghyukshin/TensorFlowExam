b1159f32718907d8635d0707abbc375edb7f8e9f
shin <auseunghyuk@gmail.com>
2019-01-28 11:45:19 +0900

    clean up files

---
+++ lab04/LoadingDataFromFile.py
@@ -0,0 +1,13 @@
+import  os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import
+import numpy as np
+
+xy = np.loadtxt('data-01-test-score.csv', delimiter=',', dtype=np.float32)
+x_data = xy[:, :-1]
+y_data = xy[:,[-1]]
+
+print(x_data.shape, x_data, len(x_data))
+print(y_data.shape, y_data)
+
+X = tf.placeh
